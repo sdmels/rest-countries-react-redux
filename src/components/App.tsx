e484abb5193c6ec21085dbb17f575d4f1a6d3f86
Daniel Susaimanickam <sdmels@outlook.com>
2020-10-06 22:30:07 +1100

    created components to setup routes

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import Dashboard from './Dashboard';
 
 function App() {
-  return <div className="App">app</div>;
+  return (
+    <>
+      <header>Header</header>
+      <main>
+        <Dashboard />
+      </main>
+      <footer>footer</footer>
+    </>
+  );
 }
 
 export default App;
