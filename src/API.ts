9a0d90eb5739948b06323a83a22098786488aff7
Daniel Melvin Susaimanickam <sdmels@outlook.com>
2022-08-03 20:48:10 +1000

    Update API.ts

--- src/API.ts
+++ src/API.ts
@@ -2,6 +2,6 @@ import axios from 'axios';
 
 export const getCountries = () => {
   return axios.get(
-    `https://restcountries.com/rest/v2/all?fields=alpha3Code;topLevelDomain;name;capital;region;population;flag;subregion;nativeName;currencies;languages;borders;alpha2Code`,
+    `https://restcountries.com/v2/all?fields=alpha3Code;topLevelDomain;name;capital;region;population;flag;subregion;nativeName;currencies;languages;borders;alpha2Code`,
   );
 };
